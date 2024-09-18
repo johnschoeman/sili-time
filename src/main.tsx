047fcaf811205efbc27ffb0ce2c02882df564a88
John Schoeman <johnschoeman1617@gmail.com>
2024-09-18 08:50:58 -0400

    Introduce solid router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,34 @@
 import App from "./App"
+import { JSX } from "solid-js"
 
 import "./index.css"
 
 import { Option, pipe } from "effect"
 import { render } from "solid-js/web"
+import { RouteDefinition, Router } from "@solidjs/router"
+
+const About = (): JSX.Element => {
+  return <h1>about</h1>
+}
+
+const routes: RouteDefinition[] = [
+  {
+    path: "/",
+    component: App,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+]
 
 pipe(
   document.getElementById("app"),
   Option.fromNullable,
   Option.match({
-    onNone: () => {},
-    onSome: r => render(() => <App />, r),
+    onNone: () => {
+      throw new Error("Unable to find root div")
+    },
+    onSome: r => render(() => <Router>{routes}</Router>, r),
   }),
 )
